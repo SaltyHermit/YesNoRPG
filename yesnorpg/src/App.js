084eacf8cafe8f29290eecf6fe26a2dc745eb7de
SaltyHermit <63828615+SaltyHermit@users.noreply.github.com>
2021-12-21 12:01:59 -0600

    Gameplay Framework

--- yesnorpg/src/App.js
+++ yesnorpg/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 function App() {
+  function yesButtonHandler() {
+    console.log("OffWithHishHEADDDDDDDAHSDFO!");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card className="card-style">
+      <div className="App">
+        <Card.Header className="card-header">
+          Scenario
+          <Card.Body>The King wants more cake</Card.Body>
+          <Card.Footer>
+            <Button onClick={yesButtonHandler}>Yes</Button>
+            <Button>No</Button>
+          </Card.Footer>
+        </Card.Header>
+      </div>
+    </Card>
   );
 }
 
